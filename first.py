1273dbc032aba1ea7e94d2788243330cc8980ec4
Veronika Semenova <nikacat2529@gmail.com>
2021-12-08 17:54:15 +0300

    lool

--- first.py
+++ first.py
@@ -1,14 +1,6 @@
-def my_superfunction():
-    print('What an awesome print!')
-
-
 def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('Again')
-    print('UFO came and added this line')
-    my_superfunction()
-
+    print('oh')
+    print('nothing to see here')
 
 if __name__ == '__main__':
     main()
